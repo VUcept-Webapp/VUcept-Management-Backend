92b761cbd7bf9f4bd8e6472d4ca9c540760d86e0
“OliviaLiu” <xiaohan.liu.1@vanderbilt.edu>
2022-10-24 21:58:11 -0500

    changed names for events and visions list

--- controllers/VUAttendanceController.js
+++ controllers/VUAttendanceController.js
@@ -293,7 +293,7 @@ exports.exportVUAttendance =  async (req, res) => {
     }
 }
 
-exports.getVisionsList =  async (req, res) => {
+exports.getVUAttendanceVisionsList =  async (req, res) => {
     const query = `SELECT DISTINCT visions from vu_attendance`;
     const getVisions = new Promise((resolve, reject) => {
       connection.query(query, (err, res) => {
@@ -314,7 +314,7 @@ exports.getVisionsList =  async (req, res) => {
     }
 }
 
-exports.getEventsList =  async (req, res) => {
+exports.getVUAttendanceEventsList =  async (req, res) => {
     const query = `SELECT DISTINCT title from vu_attendance`;
     const getEvents = new Promise((resolve, reject) => {
       connection.query(query, (err, res) => {
