ad666212db43cd8e92716f451b069827164560c8
“OliviaLiu” <xiaohan.liu.1@vanderbilt.edu>
2022-11-16 17:34:27 -0600

    fixed attendance

--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,13 @@ const routerUser = express.Router();
 const userController = require('../controllers/userController');
 
 // Routes
-// routerUser.post('/resetUsers', userController.resetUsers);
-// routerUser.post('/userLoadfromcsv', userController.userLoadfromcsv);
-// routerUser.post('/createUser', userController.createUser);
-// routerUser.post('/updateUser', userController.updateUser);
-// routerUser.post('/deleteUser', userController.deleteUser);
-// routerUser.get('/readUser', userController.readUser);
-// routerUser.get('/visionsNums', userController.visionsNums);
+routerUser.post('/resetUsers', userController.resetUsers);
+routerUser.post('/userLoadfromcsv', userController.userLoadfromcsv);
+routerUser.post('/createUser', userController.createUser);
+routerUser.post('/updateUser', userController.updateUser);
+routerUser.post('/deleteUser', userController.deleteUser);
+routerUser.get('/readUser', userController.readUser);
+routerUser.get('/visionsNums', userController.visionsNums);
 
 routerUser.get('/readUser', userController.readUser);
 
